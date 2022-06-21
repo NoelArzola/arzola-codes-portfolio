87b986fbcd9f78aca6ea1937c34f363d926fb5db
Noel Arzola <noel@skillcrush.com>
2022-06-21 13:22:02 -0400

    Move arrays to data.js

--- src/Tools.js
+++ src/Tools.js
@@ -1,53 +1,7 @@
 import React from "react";
+import { toolsArray } from "./data";
 
 const Tools = () => {
-  const toolsArray = [
-    {
-      name: "HTML5",
-      icon: '<i class="fab fa-html5" aria-hidden="true"></i>',
-    },
-    {
-      name: "CSS3",
-      icon: '<i class="fab fa-css3" aria-hidden="true"></i>',
-    },
-    {
-      name: "Javascript",
-      icon: '<i class="fab fa-js" aria-hidden="true"></i>',
-    },
-    {
-      name: "ReactJS",
-      icon: '<i class="fab fa-react" aria-hidden="true"></i>',
-    },
-    {
-      name: "Python",
-      icon: '<i class="fab fa-python" aria-hidden="true"></i>',
-    },
-    {
-      name: "NodeJS",
-      icon: '<i class="fab fa-node-js" aria-hidden="true"></i>',
-    },
-    {
-      name: "WordPress",
-      icon: '<i class="fab fa-wordpress-simple" aria-hidden="true"></i>',
-    },
-    {
-      name: "PHP",
-      icon: '<i class="fab fa-php" aria-hidden="true"></i>',
-    },
-    {
-      name: "SQL",
-      icon: '<i class="fas fa-database" aria-hidden="true"></i>',
-    },
-    {
-      name: "OpenCart",
-      icon: '<i class="fab fa-opencart" aria-hidden="true"></i>',
-    },
-    {
-      name: "Shopify",
-      icon: '<i class="fab fa-shopify" aria-hidden="true"></i>',
-    },
-  ];
-
   const displayTools = toolsArray.map((tool, index) => (
     <li key={index}>
       {tool.icon}
---
+++ src/data.js
@@ -0,0 +1,99 @@
+export const toolsArray = [
+  {
+    name: "HTML5",
+    icon: '<i class="fab fa-html5" aria-hidden="true"></i>',
+  },
+  {
+    name: "CSS3",
+    icon: '<i class="fab fa-css3" aria-hidden="true"></i>',
+  },
+  {
+    name: "Javascript",
+    icon: '<i class="fab fa-js" aria-hidden="true"></i>',
+  },
+  {
+    name: "ReactJS",
+    icon: '<i class="fab fa-react" aria-hidden="true"></i>',
+  },
+  {
+    name: "Python",
+    icon: '<i class="fab fa-python" aria-hidden="true"></i>',
+  },
+  {
+    name: "NodeJS",
+    icon: '<i class="fab fa-node-js" aria-hidden="true"></i>',
+  },
+  {
+    name: "WordPress",
+    icon: '<i class="fab fa-wordpress-simple" aria-hidden="true"></i>',
+  },
+  {
+    name: "PHP",
+    icon: '<i class="fab fa-php" aria-hidden="true"></i>',
+  },
+  {
+    name: "SQL",
+    icon: '<i class="fas fa-database" aria-hidden="true"></i>',
+  },
+  {
+    name: "OpenCart",
+    icon: '<i class="fab fa-opencart" aria-hidden="true"></i>',
+  },
+  {
+    name: "Shopify",
+    icon: '<i class="fab fa-shopify" aria-hidden="true"></i>',
+  },
+];
+
+export const projectArray = [
+  {
+    title: ,
+    tools: ,
+    description: ,
+    imageURL: ,
+    codeURL: ,
+  },
+  {
+    title: ,
+    tools: ,
+    description: ,
+    imageURL: ,
+    codeURL: ,
+  },
+  {
+    title: ,
+    tools: ,
+    description: ,
+    imageURL: ,
+    codeURL: ,
+  },
+  
+]
+
+export const services = [
+  {
+    icon: ,
+    name: ,
+    description: ,
+  },
+  {
+    icon: ,
+    name: ,
+    description: ,
+  },
+  {
+    icon: ,
+    name: ,
+    description: ,
+  },
+  {
+    icon: ,
+    name: ,
+    description: ,
+  },
+  {
+    icon: ,
+    name: ,
+    description: ,
+  }
+]
