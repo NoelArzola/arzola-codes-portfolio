f327cd9fdc279f7fe4bcda517d55e82a3cc38833
Noel Arzola <noel@skillcrush.com>
2023-11-06 15:07:42 -0500

    fix: adjust teaL rendering so that it uses more space

--- src/components/MyBrew.js
+++ src/components/MyBrew.js
@@ -29,13 +29,6 @@ export const MyBrew = ({ isActive, onClick }) => {
       className={` text-white font-semibold w-full `}
       onClick={onClick}
     >
-      <div
-        className={`mb-8 py-4 text-center ${
-          isActive ? "border-b-white border-b-2" : ""
-        }`}
-      >
-        <h2>My Brew</h2>
-      </div>
       <ul className={`${isActive ? "block" : "hidden"} w-full`}>
         {displayTeaL}
       </ul>
--- src/components/TeaL.js
+++ src/components/TeaL.js
@@ -6,12 +6,27 @@ export const TeaL = () => {
   const [activeIndex, setActiveIndex] = useState(1);
 
   return (
-    <section id="teaL" className="flex text-white font-semibold mx-4">
-      <MyBrew isActive={activeIndex === 0} onClick={() => setActiveIndex(0)} />
-      <FreshTea
-        isActive={activeIndex === 1}
-        onClick={() => setActiveIndex(1)}
-      />
+    <section id="teaL" className="text-white font-semibold mx-4">
+      <div className="flex">
+        <div
+          onClick={() => setActiveIndex(0)}
+          className={`mb-8 py-4 text-center w-full ${
+            activeIndex === 0 ? "border-b-white border-b-2" : ""
+          }`}
+        >
+          <h2>My Brew</h2>
+        </div>
+        <div
+          onClick={() => setActiveIndex(1)}
+          className={`mb-8 py-4 text-center w-full ${
+            activeIndex === 1 ? "border-b-white border-b-2" : ""
+          }`}
+        >
+          <h2>Fresh Tea</h2>
+        </div>
+      </div>
+      <MyBrew isActive={activeIndex === 0} />
+      <FreshTea isActive={activeIndex === 1} />
     </section>
   );
 };
